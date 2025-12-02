3f9952afcb3064438d781a35ab6ab2afbdf6f431
cploujoux <ch.ploujoux@gmail.com>
2025-12-02 13:55:34 -0500

    chore: update dependencies

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,8 @@
 import uvicorn
 from blaxel import env
 
-port = env["BL_SERVER_PORT"]
-host = env["BL_SERVER_HOST"]
+port = env["PORT"]
+host = env["HOST"]
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host=host, port=int(port), reload=False)
